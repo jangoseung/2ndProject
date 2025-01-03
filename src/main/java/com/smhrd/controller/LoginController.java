03f294067bcc076a3512b5bbef8a7dc3019e7143
Coding-Hyeon <businessidhyeon@gmail.com>
2025-01-03 16:40:49 +0900

    user -JH

--- src/main/java/com/smhrd/controller/LoginController.java
+++ src/main/java/com/smhrd/controller/LoginController.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.smhrd.model.userDAO;
+import com.smhrd.model.userVO;
+
 @WebServlet("/LC")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -15,7 +18,26 @@ public class LoginController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		req.setCharacterEncoding("UTF-8");
 		
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		String name = req.getParameter("name");
+		String gender = req.getParameter("gender");
+		int age = Integer.parseInt(req.getParameter("age"));
+		String time = req.getParameter("time");
+		
+		userVO vo = new userVO();
+		
+		vo.setId(id);
+		vo.setPw(pw);
+		vo.setName(name);
+		vo.setGender(gender);
+		vo.setAge(age);
+		vo.setTime(time);
+		
+		userDAO dao = new userDAO();
+		int result = dao.sign(vo);
 		
 	}
 }
